a469c9cfd9e7e438233e8d51fa2fab6e5dc88f04
Zile Wu <wuzile98@outlook.com>
2023-04-25 13:19:38 -0400

    0425_06

--- js/map.js
+++ js/map.js
@@ -61,60 +61,61 @@ function showPropertiesOnMap(map) {
   // Store the layer in the map object
   map.propertiesLayer = tileLayer;
 
+
   // Search for a property by its address
-  function searchPropertyByAddress(address) {
-    let foundProperty = null;
-    let layers = tileLayer.getLayers();
-    layers.forEach(function(layer) {
-      let layerAddress = layer.feature.properties.ADDRESS;
-      if (layerAddress && layerAddress.toUpperCase() === address.toUpperCase()) {
-        foundProperty = layer.feature;
-        return;
-      }
-    });
-    return foundProperty;
-  }
+  // function searchPropertyByAddress(address) {
+  //   let foundProperty = null;
+  //   let layers = tileLayer.getLayers();
+  //   layers.forEach(function(layer) {
+  //     let layerAddress = layer.feature.properties.ADDRESS;
+  //     if (layerAddress && layerAddress.toUpperCase() === address.toUpperCase()) {
+  //       foundProperty = layer.feature;
+  //       return;
+  //     }
+  //   });
+  //   return foundProperty;
+  // }
 
-  // Show a popup with property information
-  function showPropertyInfoPopup(feature) {
-    let popupContent =
-      "<b>" +
-      feature.properties.ADDRESS +
-      "</b><br>" +
-      "Owner: " +
-      feature.properties.OWNER +
-      "<br>" +
-      "Assessed Value: $" +
-      feature.properties.AV_TOTAL.toLocaleString() +
-      "<br>";
-    L.popup()
-      .setLatLng([
-        feature.geometry.coordinates[1],
-        feature.geometry.coordinates[0],
-      ])
-      .setContent(popupContent)
-      .openOn(map);
-  }
+  // // Show a popup with property information
+  // function showPropertyInfoPopup(feature) {
+  //   let popupContent =
+  //     "<b>" +
+  //     feature.properties.ADDRESS +
+  //     "</b><br>" +
+  //     "Owner: " +
+  //     feature.properties.OWNER +
+  //     "<br>" +
+  //     "Assessed Value: $" +
+  //     feature.properties.AV_TOTAL.toLocaleString() +
+  //     "<br>";
+  //   L.popup()
+  //     .setLatLng([
+  //       feature.geometry.coordinates[1],
+  //       feature.geometry.coordinates[0],
+  //     ])
+  //     .setContent(popupContent)
+  //     .openOn(map);
+  // }
 
-  // Add event listener to the search button
-  let searchButton = document.getElementById("search-button");
-  searchButton.addEventListener("click", function () {
-    let searchInput = document.getElementById("search-input");
-    let address = searchInput.value.trim();
-    let foundProperty = searchPropertyByAddress(address);
-    if (foundProperty) {
-      // Pan and zoom to the property location
-      let latlng = L.latLng(
-        foundProperty.geometry.coordinates[1],
-        foundProperty.geometry.coordinates[0]
-      );
-      map.setView(latlng, 18);
-      // Show a popup with property information
-      showPropertyInfoPopup(foundProperty);
-    } else {
-      alert("No property found with address " + address);
-    }
-  });
+  // // Add event listener to the search button
+  // let searchButton = document.getElementById("search-button");
+  // searchButton.addEventListener("click", function () {
+  //   let searchInput = document.getElementById("search-input");
+  //   let address = searchInput.value.trim();
+  //   let foundProperty = searchPropertyByAddress(address);
+  //   if (foundProperty) {
+  //     // Pan and zoom to the property location
+  //     let latlng = L.latLng(
+  //       foundProperty.geometry.coordinates[1],
+  //       foundProperty.geometry.coordinates[0]
+  //     );
+  //     map.setView(latlng, 18);
+  //     // Show a popup with property information
+  //     showPropertyInfoPopup(foundProperty);
+  //   } else {
+  //     alert("No property found with address " + address);
+  //   }
+  // });
 }
 
 
